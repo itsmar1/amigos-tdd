82908bdc8a11e20b323efe4639f8ad56e0eecf4b
Marouane Ahal <d144013152@gmail.com>
2022-08-07 15:34:37 +0100

    implement the business logic to validate any given phone number

--- src/test/java/com/amigoscode/testing/utils/PhoneNumberValidatorTest.java
+++ src/test/java/com/amigoscode/testing/utils/PhoneNumberValidatorTest.java
@@ -1,6 +1,7 @@
 package com.amigoscode.testing.utils;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -25,4 +26,43 @@ class PhoneNumberValidatorTest {
         // Then
         assertThat(isValid).isTrue();
     }
+
+
+    @Test
+    @DisplayName("Should fail when length is bigger than 13")
+    void itShouldValidatePhoneNumberWhenIncorrectAndHasLengthBiggerThan13() {
+        // Given
+        String phoneNumber = "+447000000000";
+
+        // When
+        boolean isValid = underTest.test(phoneNumber);
+
+        // Then
+        assertThat(isValid).isTrue();
+    }
+
+
+    @Test
+    @DisplayName("Should fail when does not start with +")
+    void itShouldValidatePhoneNumberWhenDoesNotStartWithPlusSign() {
+        // Given
+        String phoneNumber = "+447000000000";
+
+        // When
+        boolean isValid = underTest.test(phoneNumber);
+
+        // Then
+        assertThat(isValid).isTrue();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
