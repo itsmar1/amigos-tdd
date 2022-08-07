21ffd6244ec54298233fd7a0e7250053ad6c2e22
Marouane Ahal <d144013152@gmail.com>
2022-08-07 14:49:52 +0100

    create the class under under test

--- src/test/java/com/amigoscode/testing/utils/PhoneNumberValidatorTest.java
+++ src/test/java/com/amigoscode/testing/utils/PhoneNumberValidatorTest.java
@@ -1,4 +1,21 @@
 package com.amigoscode.testing.utils;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 class PhoneNumberValidatorTest {
+
+    private PhoneNumberValidator underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new PhoneNumberValidator();
+    }
+
+    @Test
+    void itShouldValidatePhoneNumber() {
+        // Given
+        // When
+        // Then
+    }
 }
