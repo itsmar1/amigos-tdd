dab6cc5cdafc858ee91f39bace2467534eb82df5
Marouane Ahal <d144013152@gmail.com>
2022-08-07 23:01:51 +0100

    parametrized tests

--- src/test/java/com/amigoscode/testing/utils/PhoneNumberValidatorTest.java
+++ src/test/java/com/amigoscode/testing/utils/PhoneNumberValidatorTest.java
@@ -3,6 +3,8 @@ package com.amigoscode.testing.utils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -15,45 +17,52 @@ class PhoneNumberValidatorTest {
         underTest = new PhoneNumberValidator();
     }
 
-    @Test
-    void itShouldValidatePhoneNumber() {
-        // Given
-        String phoneNumber = "+447000000000";
+    @ParameterizedTest
+    @CsvSource({
+            "+447000000000,true",
+            "+4470000000000087, false",
+            "447000000000, false"
+    })
+    void itShouldValidatePhoneNumber(String phoneNumber, boolean expected) {
 
         // When
         boolean isValid = underTest.test(phoneNumber);
 
         // Then
-        assertThat(isValid).isTrue();
+//        assertThat(isValid).isTrue();
+//        assertThat(isValid).isEqualTo(Boolean.valueOf(expected));
+        assertThat(isValid).isEqualTo(expected);
     }
 
-
-    @Test
-    @DisplayName("Should fail when length is bigger than 13")
-    void itShouldValidatePhoneNumberWhenIncorrectAndHasLengthBiggerThan13() {
-        // Given
-        String phoneNumber = "+447000000000";
-
-        // When
-        boolean isValid = underTest.test(phoneNumber);
-
-        // Then
-        assertThat(isValid).isTrue();
-    }
+    
 
 
-    @Test
-    @DisplayName("Should fail when does not start with +")
-    void itShouldValidatePhoneNumberWhenDoesNotStartWithPlusSign() {
-        // Given
-        String phoneNumber = "+447000000000";
-
-        // When
-        boolean isValid = underTest.test(phoneNumber);
-
-        // Then
-        assertThat(isValid).isTrue();
-    }
+//    @Test
+//    @DisplayName("Should fail when length is bigger than 13")
+//    void itShouldValidatePhoneNumberWhenIncorrectAndHasLengthBiggerThan13() {
+//        // Given
+//        String phoneNumber = "+447000000000";
+//
+//        // When
+//        boolean isValid = underTest.test(phoneNumber);
+//
+//        // Then
+//        assertThat(isValid).isTrue();
+//    }
+//
+//
+//    @Test
+//    @DisplayName("Should fail when does not start with +")
+//    void itShouldValidatePhoneNumberWhenDoesNotStartWithPlusSign() {
+//        // Given
+//        String phoneNumber = "447000000000";
+//
+//        // When
+//        boolean isValid = underTest.test(phoneNumber);
+//
+//        // Then
+//        assertThat(isValid).isTrue();
+//    }
 }
 
 
